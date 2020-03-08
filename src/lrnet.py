aae2b7f4679c3ea3e9de471c1fd52cc5a27f11bc
Jan Vargovský <jan.vargovsky@gmail.com>
2020-03-08 23:28:14 +0100

    ✨ LRNet tolerance

--- src/lrnet.py
+++ src/lrnet.py
@@ -2,39 +2,32 @@ import networkx as nx
 import math
 from sklearn.metrics.pairwise import rbf_kernel
 
+tolerance = 1e-6
 
 def get_neighbors(S, o):
     for index, similarity in enumerate(S[o]):
-        if index == o:
+        if index == o or similarity < tolerance:
             continue
         yield similarity
 
 
 def get_neighbors_i(S, o):
     for index, similarity in enumerate(S[o]):
-        if index == o:
+        if index == o or similarity < tolerance:
             continue
         yield index, similarity
-
-
-def degree(S, o, threshold=0.0):
-    degree = 0
-    for similarity in get_neighbors(S, o):
-        if similarity > threshold:
-            degree += 1
-    return degree
 
 
 def calculate_representativeness(S, min_neighbors, reduction):
     objects = list(range(len(S)))
     # Local degree
-    d = [degree(S, o) for o in objects]
+    d = [sum(1 for _ in get_neighbors(S, o)) for o in objects]
     # Local significance
     g = [0] * len(objects)
     max_similarity = [max(get_neighbors(S, o)) for o in objects]
     for o in objects:
         for i, sim in get_neighbors_i(S, o):
-            if math.isclose(sim, max_similarity[o], rel_tol=1e-8):
+            if math.isclose(sim, max_similarity[o], rel_tol=tolerance):
                 g[i] += 1
     # x-representativeness base
     b = [((1 + d[o]) ** (1 / g[o]) if g[o] > 0 else 0) for o in objects]
@@ -44,12 +37,12 @@ def calculate_representativeness(S, min_neighbors, reduction):
     for o in objects:
         k = lr[o] * d[o]
         k *= reduction
-        k = max(min_neighbors, round(k))
+        k = max(min_neighbors, int(round(k)))
 
         neighbors = sorted(get_neighbors_i(S, o), key=lambda x: x[1], reverse=True)
         last = neighbors[k-1][1]
         # include neighbors with same similarity for deterministic behaviour
-        while math.isclose(last, neighbors[k][1], rel_tol=1e-8):
+        while math.isclose(last, neighbors[k][1], rel_tol=tolerance):
             k += 1
         yield o, [x[0] for x in neighbors[:k]]
 
